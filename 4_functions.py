e6857214ca8704f10dc0e8c90444d12d31a792ae
Valeria Ivanchenkova <54852259+ivlera@users.noreply.github.com>
2023-02-20 10:04:00 +0300

    Update 4_functions.py
    Bug fix: text_normalization() did not return the last letter of input text

--- 4_functions.py
+++ 4_functions.py
@@ -60,7 +60,7 @@ def text_normalization(str_var):
             list_str_var[i] = list_str_var[i].upper()
     str_var_joined = ''.join(list_str_var)
     first_letter_upper = str_var_joined[0].upper()
-    return first_letter_upper+str_var_joined[1:-1]
+    return first_letter_upper+str_var_joined[1::]
 
 
 def last_words_sentence(text):
